c215e7140c926d767b408fe3978677a014a12c1d
Chloe Zhu <zhu.c@wustl.edu>
2025-09-02 19:10:28 -0500

    YAY PRACTICE

--- src/module1/_1whyjava/E02PoemExample.java
+++ src/module1/_1whyjava/E02PoemExample.java
@@ -4,6 +4,8 @@ public class E02PoemExample {
 	//Exercise 2: Add two lines to complete the poem
 	public static void main(String[] args) {
 		System.out.println("Roses are red,");
-		System.out.println("Violets are blue,");
+		System.out.println("Cacti are prickly,");
+		System.out.println("Holy Hera,");
+		System.out.println("That escelated quickly.");
 	}
 }
